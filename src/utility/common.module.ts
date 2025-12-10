93d30802c353229fbf289d6ae5a050b0229fa65d
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2025-12-11 00:13:09 +0100

    fix module

--- src/utility/common.module.ts
+++ src/utility/common.module.ts
@@ -7,12 +7,11 @@ import { MeetingInvitee } from 'models/meeting-invitee.model';
 import { MeetingRepository } from 'repository/meeting.repository';
 import { ProjectConsultantRepository } from 'repository/project-consultant.repository';
 import { ProjectConsultant } from 'models/project-consultant.model';
-import { EmailService } from 'src/common/emails/email.util';
 
 
 @Module({
   imports: [SequelizeModule.forFeature([Meeting, MeetingInvitee, ProjectConsultant])],
   controllers: [CommonController],
-  providers: [CommonService, MeetingRepository, ProjectConsultantRepository, EmailService],
+  providers: [CommonService, MeetingRepository, ProjectConsultantRepository],
 })
 export class CommonModule {}
