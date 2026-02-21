7a32ac93a8f69274383c04c2b5f66b4aa66f52c8
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-02-21 22:03:07 +0100

    send email issue debug 2

--- src/common/emails/email.util.ts
+++ src/common/emails/email.util.ts
@@ -56,15 +56,10 @@ export async function sendEmail(
 
     // 🔹 Transporter
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: Number(process.env.MAIL_PORT),
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false, // ✅ production me sometimes needed
+        pass: process.env.MAIL_PASS, // 🔥 APP PASSWORD
       },
     });
 
