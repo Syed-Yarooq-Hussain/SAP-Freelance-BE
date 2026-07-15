c3eabe25dfa426e3a18f09737f956dfd4651b2f6
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-07-15 17:45:13 +0200

    check linkedin issue

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,65 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as dotenv from 'dotenv';
 import { AllExceptionsFilter } from './config/allexceptions.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import * as express from 'express';
 import * as path from 'path';
 import session from 'express-session';
 import * as passport from 'passport';
+import { RedisStore } from 'connect-redis';
+import { createClient } from 'redis';
 
 async function bootstrap() {
-  dotenv.config();
-
   const app = await NestFactory.create(AppModule);
 
-  // 🔥 VERY IMPORTANT FOR RAILWAY / PROXY
-  const server = app.getHttpAdapter().getInstance();
-  server.set('trust proxy', 1);
+  app.getHttpAdapter().getInstance().set('trust proxy', 1);
 
   app.enableCors({
     origin: process.env.FE_URL,
     credentials: true,
   });
 
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalFilters(new AllExceptionsFilter());
+  const redisClient = createClient({
+    url: process.env.REDIS_URL,
+  });
 
-  app.use('/pdf', express.static(path.join(process.cwd(), 'pdf')));
+  redisClient.on('error', (error) => {
+    console.error('Redis error:', error);
+  });
+
+  await redisClient.connect();
 
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || 'super-secret',
+      name: 'sap.sid',
+
+      store: new RedisStore({
+        client: redisClient,
+        prefix: 'sap:sess:',
+      }),
+
+      secret: process.env.SESSION_SECRET!,
       resave: false,
       saveUninitialized: false,
+
       cookie: {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: true, 
-        sameSite: 'none',     
+        secure: process.env.NODE_ENV === 'production',
+        sameSite:
+          process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       },
     }),
   );
 
   app.use(passport.initialize());
   app.use(passport.session());
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.use('/pdf', express.static(path.join(process.cwd(), 'pdf')));
 
   const options = new DocumentBuilder()
     .setTitle('SAP-freelance-portal-api')
@@ -55,11 +71,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT) || 3000;
+
   await app.listen(port);
 
   console.log(`App running on port ${port}`);
-  
 }
 
 bootstrap();
